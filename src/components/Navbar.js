6447bf9a7774f5dd2796d58eb6d5446fdc29b0ee
Reza Ageng <rezaageng5@gmail.com>
2022-02-06 18:33:20 +0700

    edit black color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav className="navbar bg-slate-800">
-      <ul className="flex p-3">
+    <nav className="navbar bg-slate-900">
+      <ul className="flex p-3 gap-4">
         <li className="nav-li">
           <Link to="/" className="p-2">
             Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: "rgba(12,18,32,1)",
+      },
+    },
   },
   plugins: [],
 }
