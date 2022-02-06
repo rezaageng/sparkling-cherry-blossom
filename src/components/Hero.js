6fd264008434e078167b2290824b81658988d60f
Reza Ageng <rezaageng5@gmail.com>
2022-02-06 18:18:44 +0700

    delete unused class

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+export default function Hero() {
+  return (
+    <div>
+      <div>
+        <h2>Lorem, ipsum.</h2>
+        <span>Lorem ipsum dolor sit amet.</span>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+          consequatur quae nostrum fugit distinctio, error magni voluptates
+          labore facilis voluptatibus fugiat hic repellat obcaecati aspernatur
+          ratione. Consectetur neque dignissimos dolorum.
+        </p>
+      </div>
+    </div>
+  )
+}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom"
 
 export default function Navbar() {
   return (
-    <nav className="navbar bg-slate-200 dark:bg-slate-800">
+    <nav className="navbar bg-slate-800">
       <ul className="flex p-3">
-        <li className="nav-li nav-hover">
+        <li className="nav-li">
           <Link to="/" className="p-2">
             Home
           </Link>
         </li>
-        <li className="nav-li nav-hover">
+        <li className="nav-li">
           <Link to="/credits" className="p-2">
             Credits
           </Link>
