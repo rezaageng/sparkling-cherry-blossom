0bf00d83323517b0fe8143fa251a03ca9737edac
Reza Ageng <rezaageng5@gmail.com>
2022-02-07 20:03:10 +0700

    app light/dark mode

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Routing from "./Routes"
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-slate-100 text-gray-800 dark:bg-slate-900 dark:text-slate-300 h-screen">
       <Navbar />
       <Routing />
     </div>
