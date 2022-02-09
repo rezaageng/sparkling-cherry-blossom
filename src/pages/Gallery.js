9f84fccef8799f870d017b2caa8dd3f0357599bb
Reza Ageng <rezaageng5@gmail.com>
2022-02-09 19:16:47 +0700

    add fullscreen

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,6 +4,8 @@ import { Helmet } from "react-helmet-async"
 
 export default function Gallery() {
   const [images, setImages] = useState([])
+  const [fullscreen, setFullscreen] = useState(false)
+  const [imgCute, setImgCute] = useState("")
 
   useEffect(() => {
     const booru = new Danbooru("http://safebooru.donmai.us/")
@@ -15,19 +17,46 @@ export default function Gallery() {
     getImage()
   }, [])
 
+  function closeBg(el) {
+    if (el.id !== "cuteBg") return
+
+    el.classList.toggle("hidden")
+    setFullscreen(false)
+  }
+
   return (
     <>
       <Helmet>
         <title>Gallery</title>
       </Helmet>
+      <div
+        id="cuteBg"
+        className={
+          fullscreen
+            ? "fixed top-0 left-0 z-10 w-full h-screen bg-slate-700/75 p-4 grid place-content-center"
+            : "hidden"
+        }
+        onClick={(e) => {
+          closeBg(e.target)
+        }}
+      >
+        <img
+          src={imgCute.src}
+          alt="img cute"
+          className="opacity-100 max-w-3xl"
+        />
+      </div>
       <div className="max-w-7xl m-auto columns-5 gap-4">
         {images.map((image) => (
-          <div className="rounded-3xl mb-4 overflow-hidden">
+          <div key={image.id} className="rounded-3xl mb-4 overflow-hidden">
             <img
               src={image.file_url}
               alt="katou"
-              key={image.id}
               className="transition-transform hover:scale-110 hover:cursor-pointer"
+              onClick={(e) => {
+                setFullscreen(true)
+                setImgCute(e.target)
+              }}
             />
           </div>
         ))}
