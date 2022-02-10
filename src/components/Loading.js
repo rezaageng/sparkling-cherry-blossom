ce5d358c0ae4ad1334ae101f3778d8ab134ec92c
Reza Ageng <rezaageng5@gmail.com>
2022-02-10 11:44:09 +0700

    add loading

---
+++ src/components/Loading.js
@@ -0,0 +1,9 @@
+import LoadingSVG from "../icons/LoadingSVG"
+
+export default function Loading() {
+  return (
+    <div className="max-w-7xl m-auto min-h-[75vh] grid place-content-center">
+      <LoadingSVG />
+    </div>
+  )
+}
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,12 +1,14 @@
 import Danbooru from "danbooru"
 import { useEffect, useState } from "react"
 import { Helmet } from "react-helmet-async"
+import Loading from "../components/Loading"
 
 export default function Gallery() {
   const [images, setImages] = useState([])
   const [fullscreen, setFullscreen] = useState(false)
   const [imgCute, setImgCute] = useState("")
   const [imgSrc, setImgSrc] = useState("")
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
     const booru = new Danbooru("http://safebooru.donmai.us/")
@@ -14,6 +16,7 @@ export default function Gallery() {
       const request = await booru.posts({ tags: "katou_megumi", limit: 25 })
 
       setImages(request)
+      setLoading(false)
     }
     getImage()
   }, [])
@@ -49,22 +52,26 @@ export default function Gallery() {
           />
         </a>
       </div>
-      <div className="max-w-7xl m-auto columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4">
-        {images.map((image) => (
-          <div key={image.id} className="rounded-3xl mb-4 overflow-hidden">
-            <img
-              src={image.file_url}
-              alt="katou"
-              className="transition-transform hover:scale-110 hover:cursor-pointer"
-              onClick={(e) => {
-                setFullscreen(true)
-                setImgCute(e.target)
-                setImgSrc(image.source)
-              }}
-            />
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="max-w-7xl m-auto columns-2 md:columns-3 lg:columns-4 xl:columns-5 gap-4">
+          {images.map((image) => (
+            <div key={image.id} className="rounded-3xl mb-4 overflow-hidden">
+              <img
+                src={image.file_url}
+                alt="katou"
+                className="transition-transform hover:scale-110 hover:cursor-pointer"
+                onClick={(e) => {
+                  setFullscreen(true)
+                  setImgCute(e.target)
+                  setImgSrc(image.source)
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </>
   )
 }
